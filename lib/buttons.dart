ab42fcb78d03b5dcc44d0f813c17f724ec214ab8
Lalit <lalit.pratap.naidu@gmail.com>
2021-10-24 01:35:45 -0400

    1. added dad jokes app

---
+++ lib/buttons.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Buttons extends StatelessWidget {
+  final Function(String) changeJokeIndex;
+
+  Buttons({required this.changeJokeIndex});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.all(25),
+          child: FloatingActionButton(
+            onPressed: () {
+              changeJokeIndex("prev");
+            },
+            child: Icon(
+              Icons.arrow_left_rounded,
+              size: 60,
+            ),
+            backgroundColor: Colors.orange,
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.all(25),
+          child: FloatingActionButton(
+            onPressed: () {
+              changeJokeIndex("next");
+            },
+            child: Icon(
+              Icons.arrow_right_rounded,
+              size: 60,
+            ),
+            backgroundColor: Colors.orange,
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/buttons.dart';
+import "./question.dart";
+import "./answer.dart";
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _jokes = [
+    {
+      "question": "What does a baby computer call its father?",
+      "answer": "Data"
+    },
+    {
+      "question": "What's a pencil with two erasers called?",
+      "answer": "Pointless"
+    },
+    {
+      "question": "Why is the calendar always scared?",
+      "answer": "Because it's days are numbered"
+    },
+    {"question": "What do you call a fish with no eye", "answer": "Fsh"},
+  ];
+
+  var _jokeIndex = 0;
+
+  void changeJokeIndex(String direction) {
+    if (direction == "next") {
+      if (_jokeIndex != _jokes.length - 1) {
+        setState(() {
+          _jokeIndex++;
+        });
+      } else {
+        setState(() {
+          _jokeIndex = _jokeIndex = 0;
+        });
+      }
+    } else if (direction == "prev") {
+      if (_jokeIndex != 0) {
+        setState(() {
+          _jokeIndex--;
+        });
+      } else {
+        _jokeIndex = _jokes.length - 1;
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Question(
+                question: _jokes[_jokeIndex]["question"] as String,
+              ),
+              Answer(
+                answer: _jokes[_jokeIndex]["answer"] as String,
+              ),
+              Buttons(changeJokeIndex: changeJokeIndex)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
